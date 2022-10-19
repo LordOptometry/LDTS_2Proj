7d13a09fe92169e87a6c9939bcd9603cce7f2b3c
Nuno Silva <nunosilva@MacBook-Pro-de-Nuno-4.local>
2022-10-19 17:20:58 +0100

    mutation testing covered

--- src/test/java/com/aor/numbers/PositiveFilterTest.java
+++ src/test/java/com/aor/numbers/PositiveFilterTest.java
@@ -13,7 +13,7 @@ public class PositiveFilterTest {
     void accept() {
         PositiveFilter filter = new PositiveFilter();
         List<Integer> positives = Arrays.asList(1, 2, 3, 10, 100, 1000, Integer.MAX_VALUE);
-        List<Integer> notPositives = Arrays.asList(0, -1, -2, -3, -10, -100, -1000, Integer.MIN_VALUE);
+        List<Integer> notPositives =  Arrays.asList(0, -1, -2, -3, -10, -100, -1000, Integer.MIN_VALUE);
 
         for (int n : positives)
             Assertions.assertTrue(filter.accept(n));
